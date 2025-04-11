c0a53e893f0b05e18ae6aaa9be9f64247c87b821
araji191 <113402781+araji191@users.noreply.github.com>
2025-04-11 11:39:33 -0600

    Create algorithms.h

---
+++ algorithms.h
@@ -0,0 +1,4 @@
+void fcfs(Process processes[], int n);
+void sjf_non_preemptive(Process processes[], int n);
+void sjf_preemptive(Process processes[], int n);
+void round_robin(Process processes[], int n, int quantum);
