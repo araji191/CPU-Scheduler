ac82b5829d9bcbe2555cf750c7349d66c99fb0c3
araji191 <113402781+araji191@users.noreply.github.com>
2025-04-11 11:38:43 -0600

    Update process.h

--- process.h
+++ process.h
@@ -11,9 +11,4 @@ typedef struct Process {
   int turnaround_time;
 } Process;
 
-void fcfs(Process processes[], int n);
-void sjf_non_preemptive(Process processes[], int n);
-void sjf_preemptive(Process processes[], int n);
-void round_robin(Process processes[], int n, int quantum);
-
 #endif
