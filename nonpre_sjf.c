de33d96e69fe91239f5dc860c6d652d00e58f2c3
Jacky On <jon878@mtroyal.ca>
2025-04-13 17:26:21 +0000

    helper

--- nonpre_sjf.c
+++ nonpre_sjf.c
@@ -1,50 +1,67 @@
-#include "algorithms.h"
 #include "process.h"
-
-#include <stdio.h>
+#include "algorithms.h"
 #include <limits.h>
+
+void initialize_completion_flags(int is_completed[], int n);
+int select_shortest_non_preemptive_process(Process processes[], int is_completed[], int n, int time);
+void update_gantt_chart_non_preemptive(GanttInterval intervals[], int *interval_count, Process p, int start_time);
+void finalize_non_preemptive_process(Process *p, int end_time);
 
 void sjf_non_preemptive(Process processes[], int n, GanttInterval intervals[], int *interval_count) {
     int completed = 0, time = 0;
     int is_completed[n];
     *interval_count = 0;
 
-    for (int i = 0; i < n; i++) {
-        is_completed[i] = 0;
-    }
+    initialize_completion_flags(is_completed, n);
 
     while (completed < n) {
-        int idx = -1;
-        int min_bt = INT_MAX;
-
-        // Find the shortest job that's arrived
-        for (int i = 0; i < n; i++) {
-            if (!is_completed[i] &&
-                processes[i].arrival_time <= time &&
-                processes[i].burst_time < min_bt) {
-                min_bt = processes[i].burst_time;
-                idx = i;
-            }
-        }
+        int idx = select_shortest_non_preemptive_process(processes, is_completed, n, time);
 
         if (idx == -1) {
-            time++;  // No process available yet
+            time++;
             continue;
         }
 
-        // Record Gantt interval
-        intervals[*interval_count].start_time = time;
-        intervals[*interval_count].end_time = time + processes[idx].burst_time;
-        intervals[*interval_count].process_id = processes[idx].process_id;
-        (*interval_count)++;
+        update_gantt_chart_non_preemptive(intervals, interval_count, processes[idx], time);
 
-        // Update stats
         time += processes[idx].burst_time;
-        processes[idx].completion_time = time;
-        processes[idx].turnaround_time = time - processes[idx].arrival_time;
-        processes[idx].waiting_time = processes[idx].turnaround_time - processes[idx].burst_time;
-
+        finalize_non_preemptive_process(&processes[idx], time);
         is_completed[idx] = 1;
         completed++;
     }
+}
+
+
+void initialize_completion_flags(int is_completed[], int n) {
+    for (int i = 0; i < n; i++) {
+        is_completed[i] = 0;
+    }
+}
+
+int select_shortest_non_preemptive_process(Process processes[], int is_completed[], int n, int time) {
+    int idx = -1, min_bt = INT_MAX;
+
+    for (int i = 0; i < n; i++) {
+        if (!is_completed[i] &&
+            processes[i].arrival_time <= time &&
+            processes[i].burst_time < min_bt) {
+            min_bt = processes[i].burst_time;
+            idx = i;
+        }
+    }
+
+    return idx;
+}
+
+void update_gantt_chart_non_preemptive(GanttInterval intervals[], int *interval_count, Process p, int start_time) {
+    intervals[*interval_count].start_time = start_time;
+    intervals[*interval_count].end_time = start_time + p.burst_time;
+    intervals[*interval_count].process_id = p.process_id;
+    (*interval_count)++;
+}
+
+void finalize_non_preemptive_process(Process *p, int end_time) {
+    p->completion_time = end_time;
+    p->turnaround_time = end_time - p->arrival_time;
+    p->waiting_time = p->turnaround_time - p->burst_time;
 }
--- pre_sjf.c
+++ pre_sjf.c
@@ -1,33 +1,22 @@
 #include "process.h"
-#include <stdio.h>
-#include <stdbool.h>
 #include <limits.h>
 
+void initialize_sjf_processes(Process processes[], int n);
+int select_shortest_remaining_process(Process processes[], int n, int time);
+void update_gantt_chart_sjf(GanttInterval intervals[], int *interval_count, int process_id, int time);
+void finalize_process_if_complete(Process *p, int time, int *completed);
 
 void sjf_preemptive(Process processes[], int n, GanttInterval intervals[], int *interval_count) {
     int time = 0, completed = 0;
-    int last_pid = -1;
     *interval_count = 0;
 
-    for (int i = 0; i < n; i++) {
-        processes[i].remaining_time = processes[i].burst_time;
-        processes[i].started = 0;
-    }
+    initialize_sjf_processes(processes, n);
 
     while (completed < n) {
-        int idx = -1;
-        int min_rt = INT_MAX;
-
-        for (int i = 0; i < n; i++) {
-            if (processes[i].arrival_time <= time && processes[i].remaining_time > 0 &&
-                processes[i].remaining_time < min_rt) {
-                min_rt = processes[i].remaining_time;
-                idx = i;
-            }
-        }
+        int idx = select_shortest_remaining_process(processes, n, time);
 
         if (idx == -1) {
-            time++;  // No process is ready, CPU idle
+            time++;
             continue;
         }
 
@@ -38,24 +27,51 @@ void sjf_preemptive(Process processes[], int n, GanttInterval intervals[], int *
             p->response_time = time - p->arrival_time;
         }
 
-        // Update Gantt interval
-        if (*interval_count == 0 || intervals[*interval_count - 1].process_id != p->process_id) {
-            intervals[*interval_count].start_time = time;
-            intervals[*interval_count].end_time = time + 1;
-            intervals[*interval_count].process_id = p->process_id;
-            (*interval_count)++;
-        } else {
-            intervals[*interval_count - 1].end_time++;
-        }
+        update_gantt_chart_sjf(intervals, interval_count, p->process_id, time);
 
         p->remaining_time--;
         time++;
 
-        if (p->remaining_time == 0) {
-            p->completion_time = time;
-            p->turnaround_time = time - p->arrival_time;
-            p->waiting_time = p->turnaround_time - p->burst_time;
-            completed++;
+        finalize_process_if_complete(p, time, &completed);
+    }
+}
+
+void initialize_sjf_processes(Process processes[], int n) {
+    for (int i = 0; i < n; i++) {
+        processes[i].remaining_time = processes[i].burst_time;
+        processes[i].started = 0;
+    }
+}
+
+int select_shortest_remaining_process(Process processes[], int n, int time) {
+    int idx = -1, min_rt = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        if (processes[i].arrival_time <= time &&
+            processes[i].remaining_time > 0 &&
+            processes[i].remaining_time < min_rt) {
+            min_rt = processes[i].remaining_time;
+            idx = i;
         }
+    }
+    return idx;
+}
+
+void update_gantt_chart_sjf(GanttInterval intervals[], int *interval_count, int process_id, int time) {
+    if (*interval_count == 0 || intervals[*interval_count - 1].process_id != process_id) {
+        intervals[*interval_count].start_time = time;
+        intervals[*interval_count].end_time = time + 1;
+        intervals[*interval_count].process_id = process_id;
+        (*interval_count)++;
+    } else {
+        intervals[*interval_count - 1].end_time++;
+    }
+}
+
+void finalize_process_if_complete(Process *p, int time, int *completed) {
+    if (p->remaining_time == 0) {
+        p->completion_time = time;
+        p->turnaround_time = time - p->arrival_time;
+        p->waiting_time = p->turnaround_time - p->burst_time;
+        (*completed)++;
     }
 }
